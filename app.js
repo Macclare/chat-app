d00b54d357184e43c2b20157fce782ee9321a7f2
Macclare <mbakweangela96@gmail.com>
2025-05-27 23:17:01 +0100

    updated config

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files from the chat-app directory
+app.use(express.static(path.join(__dirname)));
+
+// Optional: fallback for root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 io.on('connection', socket => {
-    console.log('User connected');
-    socket.on('disconnect', () => console.log('User disconnected'));
+  console.log('User connected');
+  socket.on('disconnect', () => console.log('User disconnected'));
 });
 
 server.listen(3000, () => console.log('Chat app running on port 3000'));
